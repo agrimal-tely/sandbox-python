a3950104049f0f66d6e702ced37efa2c550755d7
Andrea Grimaldi <agrimaldi@telynet.com>
2026-03-12 17:32:21 +0100

    Added addition logic

--- test-app.py
+++ test-app.py
@@ -1,2 +1,12 @@
+def add(a, b):
+    return a + b
+
 name = input("What is your name?")
 print(f"Hello, {name}! Welcome back to programming after some time.")
+
+print("--- Modern Python Calc ---")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = add(num1, num2)
+print(f"The sum is: {result}")
